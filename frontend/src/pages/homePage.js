13ff6dfbea46eb32a95390cf7ead1ccdf91869f9
Madusha Lakshan <madushalakshan026@gmail.com>
2024-11-22 00:38:15 +0530

    implemented homepage

---
+++ frontend/src/pages/homePage.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Link} from 'react-router-dom'; 
+import './homePage.css';
+import logo from '../assets/LionsLineup.png'
+import heroBackground from '../assets/heroimage.png'
+
+const HomePage = () => {
+    return(
+        <div className='home-page'>
+            <nav className='navbar'>
+                <div className="logo-container">
+                    <img src={logo} alt="LionsLineup Logo" className="logo" />
+                </div>
+                <ul className="nav-links">
+                    <li>
+                        <Link to="/about" className="nav-link">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" className="nav-link">Contact Us</Link>
+                    </li>
+                </ul>
+            </nav> 
+
+            {/* Hero Section */}
+            <div
+            className="hero-section"
+            style={{ backgroundImage: `url(${heroBackground})` }}
+            >
+                <div className='hero-content'>
+                    <h1 className="hero-title">
+                        <span>L</span>ions<span>L</span>ineup
+                    </h1>
+                    <p className="hero-subtitle">Sri Lankan T20 Squad Selection Predictor</p>
+                    <button className="hero-button">CHECK NOW!</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
